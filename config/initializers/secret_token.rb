51dcc03ccb4938eec19ca0d88427f1f68401b8f5
Chris Dalley <chris@collaborate.co.uk>
2013-01-19 21:33:26 +0000

    Created Collaborate application and added basic Job listing functionallity as a test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collaborate::Application.config.secret_token = 'e44d323346d7f220a7e82537827710fe9cadf1100bf3f4eef22d1499d01bb9b2090757c28d48ea65e9fdb13f460ac5c35e47bbf90d7ed0fcc466892df1134623'
